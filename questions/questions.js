0b14bcc1c8169368d50fe90c4f0c5d236a98096a
declandoherty <darkdekk@gmail.com>
2013-04-30 21:13:55 +0100

    Question score
    displaying a score

--- questions/questions.js
+++ questions/questions.js
@@ -1,15 +1,35 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to questions.";
-  };
+Questions = new Meteor.Collection("questions");
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+if (Meteor.isClient) {
+  var MAX_CHARS = 140;
+
+  Template.compose.events({
+    'submit form': function (event) {
+      var $body = $('#que-body');
+	  var $score = 1;
+      event.preventDefault();
+
+      Questions.insert({
+        body: $body.val(),
+		score: $score,
+        created_at: Date()
+      });
+      $body.val('');
+      $('#remaining').html(MAX_CHARS);
+    },
+
+    'keyup #que-body': function() {
+      $('#remaining').html(MAX_CHARS - $('#tweet-body').val().length);
     }
   });
+
+  Template.list.events({
+    'click .que-remove': function(event) {
+      Questions.update({$que-remove: {score: 1}});
+    }
+  });
+
+  Template.list.questions = Questions.find({}, {sort: {score: -1}});
 }
 
 if (Meteor.isServer) {
