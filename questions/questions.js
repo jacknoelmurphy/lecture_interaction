6ae2766a4095dc050cba4f9868e1b63caa88e07b
declandoherty <darkdekk@gmail.com>
2013-05-06 23:40:08 +0100

    Commented
    added comments and stuff

--- questions/questions.js
+++ questions/questions.js
@@ -1,32 +1,32 @@
-
+// on the client
 if (Meteor.isClient) {
 
   Meteor.startup(function () {
     Meteor.subscribe("ques");
   });
-
+	// handle the compose event
   Template.compose.events({
     'submit form': function (event) {
       var $body = $('#que-body');
 	  var $score = 1;
       event.preventDefault();
-	
+		// check if user is logged in before entering question
 	  if(!Meteor.userId()) {
 		alert("You must login to post a question.");
 		return;
 	  }
-	
+		//insert question into database collection
       Questions.insert({
 		owner: Meteor.userId(),
         body: $body.val(),
 		score: $score,
         created_at: Date()
       });
-	  
+	  // clear textarea after create
 	  $body.val('');
     }
   });
-  
+  // deals with selected question
   Template.question.selected = function () {
     return Session.equals("selected_question", this._id) ? "selected" : '';
   };
@@ -44,21 +44,25 @@ if (Meteor.isClient) {
   
 	// Deals with up-vote, down-vote, remove buttons
   Template.list.events({
+	// thumbs up - increment score
     'click .icon-thumbs-up': function(event) {
       Questions.update(Session.get("selected_question"), {$inc: {score: 1}});
     },
+	// thumbs down - decrement score
 	'click .icon-thumbs-down': function(event) {
       Questions.update(Session.get("selected_question"), {$inc: {score: -1}});
 	},
+	// remove selected question
 	'click .icon-remove': function(event) {
       Questions.remove(Session.get("selected_question"));
     }
   });
   
-  
+  // sort in descending score, then time created
   Template.list.questions = Questions.find({}, {sort: {score: -1, created_at: -1}});
 }
 
+// on the server
 if (Meteor.isServer) {
   Meteor.startup(function () {
     Meteor.publish("ques", function(){
